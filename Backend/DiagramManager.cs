1324ccd2ea58ad5510b615ac2d1773c85f8054ac
Ciaron Howell <ciaron_howell@hotmail.co.uk>
2021-04-01 12:14:50 +0100

    Some tidying up

--- Backend/DiagramManager.cs
+++ Backend/DiagramManager.cs
@@ -3,79 +3,82 @@ using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
 
-public class DiagramManager 
+namespace FinalYearProject.Backend
 {
-
-    /// <summary>
-    /// Save diagram in directory selected in File Dialog
-    /// </summary>
-    /// <param name="xml"></param>
-    public static void SaveDiagram(string xml) 
+    public class DiagramManager
     {
-        // Configure dialog
-        SaveFileDialog saveFileDialog = new()
-        {
-            Filter = "Diagram|*.bpmn",
-            FilterIndex = 1,
-            Title = "Save a Diagram",
-            RestoreDirectory = true
-        };
 
-        // Open the file dialog to let user select where they want to save diagram
-        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+        /// <summary>
+        /// Save diagram in directory selected in File Dialog
+        /// </summary>
+        /// <param name="xml"></param>
+        public static void SaveDiagram(string xml)
         {
-            Debug.WriteLine($"SaveDiagramTest: {saveFileDialog.FileName}");
-
-            Stream stream;
-            if ((stream = saveFileDialog.OpenFile()) != null)
+            // Configure dialog
+            SaveFileDialog saveFileDialog = new()
             {
-                // Write XML string to file
-                using (StreamWriter writer = new(stream))
-                {
-                    writer.Write(xml);
-                }
+                Filter = "Diagram|*.bpmn",
+                FilterIndex = 1,
+                Title = "Save a Diagram",
+                RestoreDirectory = true
+            };
 
-                stream.Close();
+            // Open the file dialog to let user select where they want to save diagram
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Debug.WriteLine($"SaveDiagramTest: {saveFileDialog.FileName}");
+
+                Stream stream;
+                if ((stream = saveFileDialog.OpenFile()) != null)
+                {
+                    // Write XML string to file
+                    using (StreamWriter writer = new(stream))
+                    {
+                        writer.Write(xml);
+                    }
+
+                    stream.Close();
+                }
             }
         }
-    }
 
-    /// <summary>
-    /// Gets diagram XML using File Dialog
-    /// </summary>
-    /// <returns>XML </returns>
-    public static string GetDiagramXML() 
-    {
-        // Configure dialog
-        OpenFileDialog openFileDialog = new OpenFileDialog
+        /// <summary>
+        /// Gets diagram XML using File Dialog
+        /// </summary>
+        /// <returns>XML </returns>
+        public static string GetDiagramXML()
         {
-            //InitialDirectory = "" // TODO: Add home directory
-            Filter = "Diagram|*.bpmn",
-            FilterIndex = 1,
-            Title = "Open a Diagram",
-            RestoreDirectory = true
-        };
-
-        string diagram = string.Empty;
-        // Open the file dialog to let user select the diagram they want to load
-        if (openFileDialog.ShowDialog() == DialogResult.OK)
-        {
-            Stream stream;
-            if ((stream = openFileDialog.OpenFile()) != null)
+            // Configure dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog
             {
-                using (StreamReader reader = new(stream))
+                //InitialDirectory = "" // TODO: Add home directory
+                Filter = "Diagram|*.bpmn",
+                FilterIndex = 1,
+                Title = "Open a Diagram",
+                RestoreDirectory = true
+            };
+
+            string diagram = string.Empty;
+            // Open the file dialog to let user select the diagram they want to load
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Stream stream;
+                if ((stream = openFileDialog.OpenFile()) != null)
                 {
-                    diagram = reader.ReadToEnd();
+                    using (StreamReader reader = new(stream))
+                    {
+                        diagram = reader.ReadToEnd();
+                    }
+
+                    stream.Close();
                 }
 
-                stream.Close();
+                Debug.WriteLine($"GetDiagramTest: Diagram - {diagram}");
             }
 
-            Debug.WriteLine($"GetDiagramTest: Diagram - {diagram}");
+            return diagram;
         }
 
-        return diagram;
+        //TODO: Might need to add a newDiagram method to keep everything in check
     }
-
-    //TODO: Might need to add a newDiagram method to keep everything in check
 }
