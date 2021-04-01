d1c0ae8df5b5a3762838c1af14e11ef77259d5f6
Ciaron Howell <ciaron_howell@hotmail.co.uk>
2021-04-01 22:06:20 +0100

    Fixed event handler duplication + added exception catching

--- UserInterface/src/scripts/navHandler.js
+++ UserInterface/src/scripts/navHandler.js
@@ -11,12 +11,3 @@ function openCloseNavBar () {
     // Alternate between open and closed
     document.getElementById("nav").style.width = currentWidth === "250px" ? "0px" : "250px"
 }
-
-// // Handles all clicks buttons within the project nav
-// const notesNav = document.getElementById('nav');
-// notesNav.addEventListener('click', async (event) => {
-//     event.preventDefault()
-
-//     console.log("created event listener for button clicks");
-
-// });
