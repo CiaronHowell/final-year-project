15e7a1323abb77a158dac02e568cfd8ff0db573d
Ciaron Howell <ciaron_howell@hotmail.co.uk>
2021-04-16 12:13:06 +0100

    Added comments and made Window accessible everywhere to send messages to GUI

---
+++ Backend/Utils/Window.cs
@@ -0,0 +1,21 @@
+﻿using PhotinoNET;
+
+namespace FinalYearProject.Backend.Utils
+{
+    /// <summary>
+    /// Window
+    /// </summary>
+    public class Window
+    {
+        public static PhotinoWindow CurrentWindow { get; set; }
+
+        public static void SendMessage(string command, string args = null)
+        {
+            command += string.IsNullOrWhiteSpace(args) ? "" : $",{args}";
+
+            CurrentWindow.SendWebMessage(command);
+        }
+
+        // TODO: Add sendmessage that will use the current window
+    }
+}
