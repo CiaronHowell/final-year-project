2f0b5d1e5b57924fe15c04660c073589fae97978
Ciaron Howell <ciaron_howell@hotmail.co.uk>
2021-04-21 22:00:01 +0100

    One way of sharing info using Context

--- UserInterface/src/App.js
+++ UserInterface/src/App.js
@@ -1,24 +1,29 @@
+/////////// NPM Imports ////////////////
 import React from 'react';
 
 ////////// React Components ///////////////
 import BpmnModelerComponent from './components/bpmnModelerComponent';
 import WorkflowControlsComponent from './components/workflowControlsComponent';
+import ContextProvider from './components/contextComponent';
 
 ////////// CSS Imports ////////////
 import './css/App.css';
 
 class App extends React.Component {
 
-  render() {
-    return (
-      <div className="App">
-        <div id="main" className="content">
-          <WorkflowControlsComponent/>
-          <BpmnModelerComponent/>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div id="main" className="content">
+                <ContextProvider>
+                    <WorkflowControlsComponent/>
+                    <BpmnModelerComponent/>
+                </ContextProvider>
+                
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
